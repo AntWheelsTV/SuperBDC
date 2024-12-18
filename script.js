cd726ff71aa874ff4502c464dd0887891cfb51db
linhnh1 <linhnh1@younetgroup.com>
2024-12-18 18:23:13 +0700

    feat: add videos and form UI

--- script.js
+++ script.js
@@ -6,34 +6,23 @@ window.addEventListener("load", () => {
     easing: "ease-in-out",
   });
 
-  function addToggle(name, event = "click") {
-    const items = document.querySelectorAll(`.${name}-item`);
-
-    items?.forEach((item) => {
-      item.addEventListener(event, () => {
-        const current = document.querySelector(`.${name}-item.active`);
-
-        if (item !== current) {
-          item.classList.add("active");
-          current.classList.remove("active");
-        }
-      });
-    });
-  }
-
   addToggle("feature", "mouseover");
   addToggle("solution", "mouseover");
   addToggle("mining", "mouseover");
   addToggle("faq");
 
   const header = document.querySelector("header");
-
+  const hamburgerBtn = document.getElementById("hamburger");
   let lastScrollY = 0;
   window.addEventListener("scroll", () => {
     const currentScrollY = window.scrollY;
 
     if (currentScrollY > lastScrollY) {
       header.classList.add("hide");
+
+      if (hamburgerBtn.checked) {
+        hamburgerBtn.checked = false;
+      }
     } else {
       header.classList.remove("hide");
     }
@@ -42,20 +31,35 @@ window.addEventListener("load", () => {
   });
 
   const buttons = document.querySelectorAll('[data-modal="demo-modal"]');
-
   buttons.forEach((button) => {
-    button.addEventListener("click", openDialog);
+    button.addEventListener("click", openDemoModal);
   });
 });
 
-function openDialog(e) {
+function openDemoModal(e) {
   e.preventDefault();
-  // document.getElementById("demo-modal").classList.add("show");
+  // const name = e.target.dataset.modal;
+  // document.getElementById(name).classList.add("show");
   // document.body.style.overflow = "hidden";
   window.open("https://www.wheelstv.net/book-a-demo/", "_blank");
 }
 
-function closeDialog() {
-  document.getElementById("demo-modal").classList.remove("show");
+function closeDemoModal(name) {
+  document.getElementById(name).classList.remove("show");
   document.body.style.overflow = "auto";
+}
+
+function addToggle(name, event = "click") {
+  const items = document.querySelectorAll(`.${name}-item`);
+
+  items?.forEach((item) => {
+    item.addEventListener(event, () => {
+      const current = document.querySelector(`.${name}-item.active`);
+
+      if (item !== current) {
+        item.classList.add("active");
+        current.classList.remove("active");
+      }
+    });
+  });
 }
